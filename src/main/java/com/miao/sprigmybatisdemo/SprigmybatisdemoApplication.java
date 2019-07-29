369a9abd0b95e68ab4669cc364ca08714d4299bf
miaoshaodong <miaoshaodong@ruijie.com.cn>
2019-07-29 16:28:57 +0800

    miao1

--- src/main/java/com/miao/sprigmybatisdemo/SprigmybatisdemoApplication.java
+++ src/main/java/com/miao/sprigmybatisdemo/SprigmybatisdemoApplication.java
@@ -8,6 +8,7 @@ public class SprigmybatisdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SprigmybatisdemoApplication.class, args);
+        System.out.println("1213");
     }
 
 }
