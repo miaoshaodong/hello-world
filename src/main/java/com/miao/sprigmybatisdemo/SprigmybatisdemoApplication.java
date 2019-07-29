a90330bd3903eefae9b77f3b586049c3c4f9357e
miaoshaodong <miaoshaodong@ruijie.com.cn>
2019-07-29 16:19:52 +0800

    miao1

---
+++ src/main/java/com/miao/sprigmybatisdemo/SprigmybatisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.miao.sprigmybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigmybatisdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprigmybatisdemoApplication.class, args);
+    }
+
+}
